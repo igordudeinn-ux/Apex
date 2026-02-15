d959a0091257ce22f5e90bbac599a1f6c9345989
xfyder <igordudeinn@gmail.com>
2026-02-15 18:00:59 +0800

    Compile Main.py to an exe file
    How to compile: 
    py -m pip install pyinstaller 
    pyinstaller --onefile main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys, time, os
+os.system('color')
+from a_engine import Apix_Compiler
+from debugger import ApixDebugger as deb
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: py main.py <filename>.a")
+        return
+    start_time = time.time()
+    deb.log_system("Initializing Apix Engine...")
+    compiler = Apix_Compiler()
+    compiler.run_file(sys.argv[1])
+    end_time = time.time()
+    execution_time = round((end_time - start_time) * 1000, 2)
+    deb.log_system(f"Execution finished in {execution_time}ms")
+if __name__ == "__main__":
+    main()
